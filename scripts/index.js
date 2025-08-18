4225ab0d952fd99932c0c14c856975e7326e2fbe
stu102871 <102871@glr.nl>
2025-08-18 14:35:02 +0200

    Index Nav Completion

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,8 @@ const output = {
     text     : [document.getElementById("choiceTitle"), document.getElementById("choiceSubtitle")],
     boxes    : [],
     main     : document.getElementsByTagName("main")[0],
-    container: document.getElementById("contentDiv")
+    container: document.getElementById("contentDiv"),
+    fadeTimer: undefined
 }
 for (let i=0, boxClass = document.getElementsByClassName("selectorOptions"); i < boxClass.length; i++) output.boxes.push(boxClass[i]);
 
@@ -21,58 +22,47 @@ const nav = {
 
 
 const games = {
-    base : {
-        name: {
-            l: ["Battle Sim", "PokeGuesser", "Tabletop"],
-            s: ["battle", "pkGuess", "ttrpg"]
-        },
-        mode: {
-            l: ["Gen 1", "Gen 2", "Gen 3", "Gen 4", "Gen 5", "Gen 6", "Gen 7", "Gen 8", "Gen 9", "All Gens"],
-            s: ["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "All"],
-            c: 0
-        }
+    name: {
+        l: ["Battle Sim", "PokeGuesser", "Tabletop"],
+        s: ["battle", "pkGuess", "ttrpg"],
     },
-    order: [],
+    mode: {
+        l: ["Gen 1", "Gen 2", "Gen 3", "Gen 4", "Gen 5", "Gen 6", "Gen 7", "Gen 8", "Gen 9", "All Gens"],
+        s: ["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "All"],
+        c: 0
+    },
 
     display: function(change) {
 
-        // Initialize Load Order
-        if (!this.order.length) {
-
-            const center = Math.ceil(this.base.name.l.length);
-            for (let i = center-2; i < i < center+2; i++) {
-
-                if (this.base.name.l[i]) this.order.push(i);
-                else this.order.push(-1);
-            }
-
-        }
-
         // Change Load Order if necessary
-        if      (change == -1) this.order.unshift(this.order.pop());
-        else if (change == 1)  this.order.push(this.order.shift());
-        
-        // Update Boxes
-        for (let i=0; i < 5; i++) {
-
-            let path = (this.base.name.s[this.order[i]] !== "") ? `images/homepage/${this.base.name.s[this.order[i]]}.png` : `images/homepage/static.gif`;
-            output.boxes[i].style.backgroundImage = `url(${path})`;
+        if (change == -1) {
+            this.name.l.unshift(this.name.l.pop());
+            this.name.s.unshift(this.name.s.pop());
+        }
+        else if (change == 1) {
+            this.name.l.push(this.name.l.shift());
+            this.name.s.push(this.name.s.shift());
         }
 
-        // Display Game Info
-        output.text[0].innerHTML = this.base.name.l[this.order[2]];
-        output.text[1].innerHTML = this.base.mode.l[0];
+        const center = Math.floor((this.name.l.length-1)/2);
+        for (let boxI=0, infoI = center-2; boxI < output.boxes.length; boxI++, infoI++) {
+
+            // Display Text
+            output.text[0].innerHTML = this.name.l[center];
+            output.text[1].innerHTML = this.mode.l[this.mode.c];
+
+            // Display Images
+            const path = (this.name.s[infoI] !== undefined) ? `images/homepage/${this.name.s[infoI]}.png` : `images/homepage/static.gif`;
+            output.boxes[boxI].style.backgroundImage = `url(${path})`;
+        }
         
     },
 
     launch: function() {
 
-        location.href = `pages/${games.base.name.s[2]}.html?mode=${this.base.mode[cMode]}`;
+        location.href = `pages/${this.name.s[Math.floor((this.name.l.length-1)/2)]}.html?mode=${this.mode.s[this.mode.c]}`;
     }
 }
-
-
-let fadeTimer;
 
 
 
@@ -80,7 +70,7 @@ let fadeTimer;
 
 function fade(type) {
 
-    if (window.innerWidth > 800) clearTimeout(fadeTimer);
+    if (innerWidth > 850) clearTimeout(output.fadeTimer);
 
     if (type == "in") {
         output.container.style.opacity = 1;
@@ -109,16 +99,16 @@ document.addEventListener("keydown", (event) => {
     else if (event.key === "ArrowRight") games.display(1);
     else if (event.key === "ArrowUp") {
         
-        if (games.base.mode.c == 0) games.base.mode.c = games.base.mode.l.length-1;
-        else games.base.mode.c--;
-        output.text[1].innerHTML = games.base.mode.l[games.base.mode.c];
+        if (games.mode.c == 0) games.mode.c = games.mode.l.length-1;
+        else games.mode.c--;
+        output.text[1].innerHTML = games.mode.l[games.mode.c];
 
     }
     else if (event.key === "ArrowDown") {
         
-        if (games.base.mode.c == games.base.mode.l.length-1) games.base.mode.c = 0;
-        else games.base.mode.c++;
-        output.text[1].innerHTML = games.base.mode.l[games.base.mode.c];
+        if (games.mode.c == games.mode.l.length-1) games.mode.c = 0;
+        else games.mode.c++;
+        output.text[1].innerHTML = games.mode.l[games.mode.c];
 
     }
 });
@@ -127,15 +117,15 @@ document.addEventListener("keydown", (event) => {
 // UI Fade In/Out
 document.addEventListener("mouseover", (event) => {
 
-    if (window.innerWidth > 800 && (output.container.contains(event.target) || document.getElementsByTagName("header")[0].contains(event.target))) fade("in");
+    if (innerWidth > 850 && (output.container.contains(event.target) || document.getElementsByTagName("header")[0].contains(event.target))) fade("in");
 });
 document.addEventListener("mouseout", (event) => {
 
-    if (window.innerWidth > 800 && (output.container.contains(event.target) || document.getElementsByTagName("header")[0].contains(event.target))) fadeTimer = setTimeout(() => fade("out"), 1000);
+    if (innerWidth > 850 && (output.container.contains(event.target) || document.getElementsByTagName("header")[0].contains(event.target))) output.fadeTimer = setTimeout(() => fade("out"), 1000);
 });
 document.addEventListener("click", (event) => {
 
-    if (window.innerWidth <= 800) {
+    if (innerWidth <= 850) {
 
         if (output.container.contains(event.target) || document.getElementsByTagName("header")[0].contains(event.target)) fade("in");
         else fade("out");
@@ -143,7 +133,6 @@ document.addEventListener("click", (event) => {
     }
 });
 
-
 /* Function Startup */
 
-games.display(-0);
+games.display();
